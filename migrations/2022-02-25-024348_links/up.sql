3a0d28f470966c86c03d39bec3809799666c1da2
Facu <urionafacu@gmail.com>
2022-02-25 00:19:47 -0300

    :rocket: added models

---
+++ migrations/2022-02-25-024348_links/up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE links (
+  id INTEGER NOT NULL PRIMARY KEY,
+  link TEXT NOT NULL,
+  title TEXT NOT NULL,
+  date_created TEXT NOT NULL
+)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, Error, HttpResponse, HttpServer, web};
+use actix_web::http::{StatusCode};
+
+async fn home() -> Result<HttpResponse, Error> {
+    Ok(
+        HttpResponse::build(StatusCode::OK)
+            .content_type("text/html; charset=utf-8")
+            .body(include_str!("../templates/index.html"))
+    )
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(move || {
+        App::new()
+            .route("/", web::get().to(home))
+    })
+    .bind("127.0.0.1:3000")?
+    .run()
+    .await
 }
